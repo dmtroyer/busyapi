e476c874e9a28707cdfbe6518d80d68654f63876
David Troyer <dmtroyer@gmail.com>
2018-08-06 14:15:19 -0400

    using redis for usage data persistance

--- routes/api/usages.js
+++ routes/api/usages.js
@@ -1,12 +1,18 @@
+var debug = require('debug')('busyapi:server');
+var redis = require('redis');
+var client = redis.createClient();
+
 module.exports = function(app){
     app.post('/api/usages', function(req, res){
+        // Increment the last usage ID in Redis
+        client.incr('lastUsageId', function (err, reply) {
+            // Store the usage data at the incremented ID.
+            var usageId = reply;
+            client.set(usageId, JSON.stringify(req.body), function() {
+                debug('Stored usage count: ' + usageId);
+                res.status(201).json({ 'id': usageId });
+            });
+        });
 
-        // Store the supplied usage data
-        app.usages.push(req.body);
-
-        var usageId = app.usages.length;
-        console.log('Stored usage count: ' + usageId);
-
-        res.status(201).json({'id':usageId});
     });
 }
